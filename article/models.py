3924a41f23f9e56813a52803a026e5882cd8357f
lynndotconfig <lynn.config@gmail.com>
2016-01-05 12:53:36 +0800

    [fix] .gitignore

--- article/models.py
+++ article/models.py
@@ -2,16 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 class Article(models.Model):
-	title=models.CharField(max_length=100)#
-	category=models.CharField(max_length=50,blank=True)
-	date_time=models.DateTimeField(auto_now_add=True)
-	content = models.TextField(blank = True, null = True)
-	def _unicode_(self):
-	  return self.title
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, blank=True)
+    date_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=True, null=True)
 
-	  class Meta:
-	  	ordering = ['-date_time']
-    
+    def _unicode_(self):
+        return self.title
 
+    class Meta:
+        ordering = ['-date_time']
