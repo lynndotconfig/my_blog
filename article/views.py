77a6d87ba61f8998326e6b7f06b21b6c605f3201
lynndotconfig <lynn.config@gmail.com>
2016-01-05 18:21:10 +0800

    [add] simple template

--- article/views.py
+++ article/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 from django.http import HttpResponse
 from article.models import Article
 
@@ -9,7 +10,11 @@ def home(request):
 
 
 def detail(request, my_args):
-    post = Article.objects.all()[int(my_args)]
+    post = Article.objects.all()[0]
     string = ("title=%s, category=%s, date_time=%s, content=%s" % (
         post.title, post.category, post.date_time, post.content))
     return HttpResponse(string)
+
+
+def test(request):
+    return render(request, 'test.html', {'current_time': datetime.now()})
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'article.views.home'),
     url(r'^(?P<my_args>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^test/$', 'article.views.test'),
 ]
