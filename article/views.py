94598a0ab90a3fd7ad8ae5ecb2c5851d26c1c978
lynndotconfig <lynn.config@gmail.com>
2016-01-05 14:51:56 +0800

    [add] load article app

--- article/views.py
+++ article/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from article.models import Article
 
 
 # Create your views here.
@@ -8,4 +9,7 @@ def home(request):
 
 
 def detail(request, my_args):
-    return HttpResponse("You're looking at my_args %s." % my_args)
+    post = Article.objects.all()[int(my_args)]
+    string = ("title=%s, category=%s, date_time=%s, content=%s" % (
+        post.title, post.category, post.date_time, post.content))
+    return HttpResponse(string)
