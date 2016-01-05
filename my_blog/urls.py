ea7e1d2a5677f6b8d83f8f2d8bd8eec01ae89083
lynndotconfig <lynn.config@gmail.com>
2016-01-05 14:44:30 +0800

    [add] add my_args to url

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'article.views.home'),
+    url(r'^(?P<my_args>\d+)/$', 'article.views.detail', name='detail'),
 ]
