c00ba59cac15fffb778c410e56b6d884926bce74
lynndotconfig <lynn.config@gmail.com>
2016-01-07 21:17:35 +0800

    [add] try to add class view

--- article/views.py
+++ article/views.py
@@ -5,55 +5,68 @@ from article.models import Article
 from django.http import Http404
 from django.contrib.syndication.views import Feed
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import View
 
 
 # Create your views here.
-def home(request):
-    posts = Article.objects.all()
-    paginator = Paginator(posts, 2)
-    page = request.GET.get('page')
-    try:
-        post_list = paginator.page(page)
-    except PageNotAnInteger:
-        post_list = paginator.page(1)
-    except EmptyPage:
-        post_list = paginator.paginator(paginator.num_pages)
-    return render(request, 'home.html', {'post_list': post_list})
+class HomeView(View):
+
+    def get(self, request):
+        posts = Article.objects.all()
+        paginator = Paginator(posts, 2)
+        page = request.GET.get('page')
+        try:
+            post_list = paginator.page(page)
+        except PageNotAnInteger:
+            post_list = paginator.page(1)
+        except EmptyPage:
+            post_list = paginator.paginator(paginator.num_pages)
+        return render(request, 'home.html', {'post_list': post_list})
 
 
-def detail(request, id):
-    try:
-        post = Article.objects.get(id=str(id))
-    except Article.DoesNotExist:
-        raise Http404
-    return render(request, 'post.html', {'post': post})
+class DetailView(View):
+
+    def request(self, request):
+        try:
+            post = Article.objects.get(id=str(id))
+        except Article.DoesNotExist:
+            raise Http404
+        return render(request, 'post.html', {'post': post})
 
 
-def archives(request):
-    try:
-        post_list = Article.objects.all()
-    except Article.DoesNotExist:
-        raise Http404
-    return render(request, 'archives.html', {
-        'post_list': post_list, 'error': False})
+class ArchivesView(View):
+
+    def get(self, request):
+        try:
+            post_list = Article.objects.all()
+        except Article.DoesNotExist:
+            raise Http404
+        return render(request, 'archives.html', {
+            'post_list': post_list, 'error': False})
 
 
-def search_tag(request):
-    return render(request, 'home.html')
+class SearchTagView(View):
+
+    def get(self, request):
+        return render(request, 'home.html')
 
 
-def blog_search(request):
-    if 's' in request.GET:
-        s = request.GET['s']
-        if not s:
-            return render(request, 'home.html')
-        post_list = Article.objects.filter(title__icontains=s)
-        if len(post_list) == 0:
+class BlogSearchView(View):
+
+    def get(self, request):
+        import pdb
+        pdb.set_trace()
+        if 's' in request.GET:
+            s = request.GET['s']
+            if not s:
+                return render(request, 'home.html')
+            post_list = Article.objects.filter(title__icontains=s)
+            if len(post_list) == 0:
+                return render(request, 'archives.html', {'post_list': post_list,
+                                                         'error': True})
             return render(request, 'archives.html', {'post_list': post_list,
-                                                     'error': True})
-        return render(request, 'archives.html', {'post_list': post_list,
-                                                 'error': False})
-    return redirect('/')
+                                                     'error': False})
+        return redirect('/')
 
 
 class RSSFeed(Feed):
