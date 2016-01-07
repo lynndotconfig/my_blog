1b5bcafc70a7582dfebe5df297459788698e42c2
lynndotconfig <lynn.config@gmail.com>
2016-01-07 20:40:12 +0800

    [add] page more

--- article/views.py
+++ article/views.py
@@ -4,11 +4,20 @@ from django.http import HttpResponse
 from article.models import Article
 from django.http import Http404
 from django.contrib.syndication.views import Feed
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def home(request):
-    post_list = Article.objects.all()
+    posts = Article.objects.all()
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.paginator(paginator.num_pages)
     return render(request, 'home.html', {'post_list': post_list})
 
 
