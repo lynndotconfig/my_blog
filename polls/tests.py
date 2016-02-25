6e6b79aa3251318a418c38623a01c938d4fac57c
lynndotconfig <lynn.config@gmail.com>
2016-02-25 17:28:30 +0800

    [add] other tests for polls

--- polls/tests.py
+++ polls/tests.py
@@ -19,3 +19,23 @@ class QuestionMethodTest(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """Test for old question 30 days ago.
+
+        was_published_recently() should be return False
+        for question whose pub_date is 30 days ago.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_rencently_question(self):
+        """Test for recently question 1 hour ago.
+
+        was_published_recently() should be return True
+        for question whose pub_date is 1 hour ago.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recently_question = Question(pub_date=time)
+        self.assertEqual(recently_question.was_published_recently(), True)
