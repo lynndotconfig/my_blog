6e6777f5433f6131b00cea6d091ca42a768aab5e
lynndotconfig <lynn.config@gmail.com>
2016-02-25 15:42:13 +0800

    [fix] namespace urls

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 """Register model for polls admin."""
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Choice)
