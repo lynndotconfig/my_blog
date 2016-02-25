6254865785ff64225ece41a7c37b8df4c66799f1
lynndotconfig <lynn.config@gmail.com>
2016-02-25 15:06:19 +0800

    [add] polls index html

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap_admin',  # should before django.contrib.admin
+    # 'bootstrap_admin',  # should before django.contrib.admin
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,31 @@
 """views for poll model."""
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 
 # Create your views here.
 def index(request):
     """Index view for polls."""
-    return HttpResponse("Hello world, you are in poll app")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    """Detail view for poll."""
+    return HttpResponse("Your're looking at %s" % question_id)
+
+
+def result(request, question_id):
+    """Result view for polls."""
+    return HttpResponse("Yor're looking at result of %s" % question_id)
+
+
+def vote(request, question_id):
+    """Vote for poll."""
+    return HttpResponse("Your are voting on question %s" % question_id)
