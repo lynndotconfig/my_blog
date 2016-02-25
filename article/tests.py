16e425ab716db2ce65923a097925bd65e320715c
lynndotconfig <lynn.config@gmail.com>
2016-02-25 10:36:54 +0800

    [add]test for article

--- article/tests.py
+++ article/tests.py
@@ -1,3 +1,11 @@
+"""Test for article model method."""
 from django.test import TestCase
 
+
 # Create your tests here.
+class ArticleMethodTest(TestCase):
+    """Docstring for ArticleMethodTest."""
+
+    def test_article_method(self):
+        """A test try."""
+        pass
