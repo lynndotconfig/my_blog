ae6c7115db39415f3ca01ac00a69aba69adc16da
lynndotconfig <lynn.config@gmail.com>
2016-02-25 16:35:15 +0800

    [add] vote logical

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,6 @@ app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.result, name='result'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.result, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 """views for poll model."""
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.shortcuts import get_object_or_404
+from django.core.urlresolvers import reverse
 
 
 # Create your views here.
@@ -28,4 +29,22 @@ def result(request, question_id):
 
 def vote(request, question_id):
     """Vote for poll."""
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting from
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.", })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data, This prevents data being posted twice
+        # if a user hit the Back button
+        return HttpResponseRedirect(
+            reverse('polls:results', args=(question_id,)))
+    finally:
+        pass
     return HttpResponse("Your are voting on question %s" % question_id)
