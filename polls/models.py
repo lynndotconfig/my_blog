5bab457b3f732272972f3535a70a614189bc47f0
lynndotconfig <lynn.config@gmail.com>
2016-02-25 17:18:44 +0800

    [add] testcase for polls

--- polls/models.py
+++ polls/models.py
@@ -19,7 +19,8 @@ class Question(models.Model):
 
     def was_published_recently(self):
         """Check it was published recently."""
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,21 @@
+"""Test case for polls method."""
+import datetime
+from django.utils import timezone
 from django.test import TestCase
 
+from .models import Question
 # Create your tests here.
+
+
+class QuestionMethodTest(TestCase):
+    """Test qeustion methods."""
+
+    def test_was_published_recently_with_future_question(self):
+        """Function test for 'was_published_recently()'.
+
+        was_published_recently() should return False
+        for question whose pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
