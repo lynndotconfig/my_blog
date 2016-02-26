b26505e188fbc2592cc478eee89881c259be59ed
lynndotconfig <lynn.config@gmail.com>
2016-02-26 10:27:22 +0800

    [add] related model

--- polls/admin.py
+++ polls/admin.py
@@ -4,10 +4,21 @@ from .models import Question, Choice
 
 
 # Register your models here.
+class ChoiceInline(admin.StackedInline):
+    """inline choice."""
+
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     """Question Admin."""
 
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
